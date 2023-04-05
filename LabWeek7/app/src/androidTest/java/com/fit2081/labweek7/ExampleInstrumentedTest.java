9fd0c97d18707030f7d2d46cb3a03e0aae175320
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-04-06 02:49:25 +1000

    Edited LabWeek7

--- LabWeek7/app/src/androidTest/java/com/fit2081/labweek7/ExampleInstrumentedTest.java
+++ LabWeek7/app/src/androidTest/java/com/fit2081/labweek7/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.fit2081.labweek6", appContext.getPackageName());
+        assertEquals("com.fit2081.labweek7", appContext.getPackageName());
     }
 }
